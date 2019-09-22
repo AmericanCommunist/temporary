24434870b81ca3c3943e0ad5eb8152ee3d15df1a
AmericanCommunist <zeyuanjiang@gmail.com>
2019-09-22 10:32:05 -0700

    component test

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ButtonList from './components/buttons.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<ButtonList />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
